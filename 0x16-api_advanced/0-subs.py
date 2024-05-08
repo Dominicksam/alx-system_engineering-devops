7a7c367077fcb5d704424f09d02df87efe238790
Dominicksam <dominicsamakpan@gmail.com>
2024-05-08 01:12:28 -0700

    redo api ad1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,17 +3,22 @@
 Script that queries subscribers on a given Reddit subreddit.
 """
 
+
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Custom User Agent"}  # Set a custom User-Agent to prevent Too Many Requests error
+    response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        if 'data' in data and 'subscribers' in data['data']:
+            return data['data']['subscribers']
+        else:
+            # If the subreddit data doesn't contain subscriber information
+            return 0
     else:
+        # If the request was unsuccessful, return 0
         return 0
+

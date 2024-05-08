5d180d1443e7dad7d6bbb3b4801574ab9748d0b5
Dominicksam <dominicsamakpan@gmail.com>
2024-05-08 02:38:19 -0700

    adv3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,24 +5,15 @@ Script that queries subscribers on a given Reddit subreddit.
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
     if response.status_code == 200:
         data = response.json()
-        if 'data' in data and 'subscribers' in data['data']:
-            subscribers = data['data']['subscribers']
-            return f"existing subreddit: {subscribers}"
-        else:
-            # If the subreddit data doesn't contain subscriber information
-            return "existing subreddit"
-    elif response.status_code == 404:
-        # If the subreddit doesn't exist
-        return "nonexisting subreddit"
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        # If the request was unsuccessful, return 0
-        return "unknown error"
-
+        return 0
